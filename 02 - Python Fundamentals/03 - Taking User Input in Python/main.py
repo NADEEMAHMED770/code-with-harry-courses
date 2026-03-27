94b960718020a2cb806748ce36325710eee97528
NADEEM AHMED <nadeemahmedghoto06@gmail.com>
2026-03-27 21:18:30 +0500

    Delete 02 - Python Fundamentals/03 - Taking User Input in Python/main.py

--- 02 - Python Fundamentals/03 - Taking User Input in Python/main.py
+++
@@ -1,6 +0,0 @@
-age = int(input("Please enter your age: "))
-name = input("Please enter you name: ")
-if (age>=18):
-    print(name, " is eligible to vote.")
-else:
-    print(name, " is not eligible to vote.")
